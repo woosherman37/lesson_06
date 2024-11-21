0a18df9fa7b97970575d011d70651c43f85f0370
thomas0376 <thomas.prevarin@gmail.com>
2024-11-21 17:29:43 +0000

    Wrapped Match Statement into a function

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-n = "1"
+n = input(f"\nTell me a number! ")
 
-match n:
-	case 0: 
-		print("zero")
-	case 1:
-		print("one")
-	case 2:
-		print("two")
-	case _:
-		print("other")
+n = int(n)
 
+print(f"\n")
+
+def number_in_letters(num):
+
+	match num:
+		case 0: 
+			print("zero")
+		case 1:
+			print("one")
+		case 2:
+			print("two")
+		case _:
+			print("other")
+
+number_in_letters(n)
+
+
+print(f"\n")
 
 
